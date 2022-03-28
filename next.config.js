3f44ecccfbfbe434f28c08b1547666b175f43543
Pavan Chindukuri <chindukuripavan@gmail.com>
2022-03-28 15:54:44 +0530

    fdsfsad

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
 	distDir: 'build',
+	assetPrefix: isProd ? '/crypto-message-generator/' : '',
 };
 
 module.exports = nextConfig;
