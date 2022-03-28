9ebddee9d9e97870075c93fdfb082c1bd96f6877
ghana7989 <65382745+ghana7989@users.noreply.github.com>
2022-03-28 10:00:50 +0000

    Deploying to gh-pages from  @ 22a5a74d8c56a85757673c499bd3b84d3bafb3f0 🚀

---
+++ static/DhTCgeyHN1EGZbTu1yPWh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/281-392aee002992eeb9.js","static/chunks/pages/index-9e17b68aaf84e5c9.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
